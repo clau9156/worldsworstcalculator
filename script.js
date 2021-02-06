973c41dc5235185452eb9f6a66ca26812bc24571
clau9156 <clau9156@stud.kea.dk>
2021-02-06 18:45:14 +0100

    layout from activity diagram

---
+++ script.js
@@ -0,0 +1,40 @@
+// setting up 
+
+// click "calculate"
+
+// read first number 
+
+// read second number
+
+// read operator
+
+// if else --->
+// calculate first + second 
+
+// calculate first - second
+
+// calculate first / second
+
+// calculate first * second
+
+// check if result should be rounded
+
+// if --->
+// read number of decimals
+
+// round result to num of decimals
+
+// else (both then got to) --->
+// write result into first number field
+
+// append result to end of list
+
+// scroll list to the bottom 
+
+
+
+
+
+// click "clear results"
+
+// clear list of results
