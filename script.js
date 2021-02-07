6eea05eddc79e98457e1ad44fbc8053113d09175
clau9156 <clau9156@stud.kea.dk>
2021-02-07 22:15:03 +0100

    removed one thing

--- script.js
+++ script.js
@@ -27,10 +27,7 @@ document.querySelector("#clear").addEventListener("click", e=>{
     window.location.reload();
 });
 document.querySelector("#results").scrollTo(0,1000000);
-// scroll list to the bottom 
-    // scrolling
-    // doc.qse(container).scrollTo(0,1000000)
-    // document.querySelector().scrollTo(0,1000000);
+
 
 function calculating() {
     let firstNumber = document.querySelector("#firstNumber").value;
@@ -71,7 +68,7 @@ function showResult(result) {
         let option = document.querySelector("#decimals").value;
     
 
-
+        // still working on it
         if (decimals.value === "0") {
             n = num.toFixed();
         } else if (decimals.value === "1") {
@@ -92,26 +89,6 @@ function showResult(result) {
             n = num.toFixed(8);
         }
 
-        // if (option === "0") {
-        //     let n = num.toFixed();
-        // } else if (option === "1") {
-        //     n = num.toFixed(1);
-        // } else if (option === "2") {
-        //     n = num.toFixed(2);
-        // } else if (option === "3") {
-        //     n = num.toFixed(3);
-        // } else if (option === "4") {
-        //     n = num.toFixed(4);
-        // } else if (option === "5") {
-        //     n = num.toFixed(5);
-        // } else if (option === "6") {
-        //     n = num.toFixed(6);
-        // } else if (option === "7") {
-        //     n = num.toFixed(7);
-        // } else if (option === "8") {
-        //     n = num.toFixed(8);
-        // }
-
         finalResult.textContent = result;
     } else {
         finalResult.textContent = result;
@@ -120,7 +97,7 @@ function showResult(result) {
 }
 
 
-
+// other version not gonna delete till solved
 // check if result should be rounded
 // function checkRounded() {
 //     console.log("checkRounded");
@@ -149,14 +126,13 @@ function showResult(result) {
 //     } else if (option === "8") {
 //     n = num.toFixed(8);
 //     }
-
-    
-    
+   
 //     // round result to num of decimals
 
 //     // else (both then got to) --->
 // }
 
+// then to first number
 // write result into first number field
 // function WriteFirstNumberField(answer) {
 //     // writing to field --->
@@ -164,12 +140,6 @@ function showResult(result) {
 //     document.querySelector("#firstNumber").value = result;
 // }
 
-// append result to end of list
-
-// scroll list to the bottom 
-    // scrolling
-    // doc.qse(container).scrollTo(0,1000000)
-    // document.querySelector().scrollTo(0,1000000);
 
 
 
